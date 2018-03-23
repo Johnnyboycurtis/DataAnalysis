03ae6a04941f1053fb20edf5814cf96fc8dbbb08
JN107154 <jn107154@dell.corp.int>
2018-03-23 13:47:47 -0700

    training model with GPU; pretrain first then retrain with full data

--- Image/Load-Cifar-10-CNN-mini-2.py
+++ Image/Load-Cifar-10-CNN-mini-2.py
@@ -22,48 +22,10 @@ num_train, img_channels, img_rows, img_cols =  train_features.shape
 num_test, _, _, _ =  test_features.shape
 num_classes = len(np.unique(train_labels))
 
-'''
-import pickle
-
-with open('/home/jonathan/Documents/cifar-10-batches-py/train.pickle', 'wb+') as myfile:
-    pickle.dump((train_features, train_labels), myfile)
-
-with open('/home/jonathan/Documents/cifar-10-batches-py/test.pickle', 'wb+') as myfile:
-    pickle.dump((test_features, test_labels), myfile)
-'''
-
-class_names = ['airplane','automobile','bird','cat','deer',
-               'dog','frog','horse','ship','truck']
-fig = plt.figure(figsize=(8,3))
-for i in range(num_classes):
-    ax = fig.add_subplot(2, 5, 1 + i, xticks=[], yticks=[])
-    idx = np.where(train_labels[:]==i)[0]
-    features_idx = train_features[idx,::]
-    img_num = np.random.randint(features_idx.shape[0])
-    im = np.transpose(features_idx[img_num,::], (1, 2, 0))
-    ax.set_title(class_names[i])
-    plt.imshow(im)
-plt.show()
-
-
-ind = np.random.rand(50000) < 0.35
-val_features = train_features[~ind]
-val_labels = train_labels[~ind]
-
-train_features = train_features[ind]
-train_labels = train_labels[ind]
-
-ind = np.random.rand(10000) < 0.30
-test_labels = test_labels[ind]
-test_features = test_features[ind]
-
-
 train_features = train_features.astype('float32')/255
-val_features = val_features.astype('float32')/255
 test_features = test_features.astype('float32')/255
 # convert class labels to binary class labels
 train_labels = np_utils.to_categorical(train_labels, num_classes)
-val_labels = np_utils.to_categorical(val_labels, num_classes)
 test_labels = np_utils.to_categorical(test_labels, num_classes)
 
 
@@ -89,45 +51,45 @@ def plot_model_history(model_history):
     plt.show()
 
 
+
+
+
+
 def accuracy(test_x, test_y, model):
     result = model.predict(test_x)
     predicted_class = np.argmax(result, axis=1)
     true_class = np.argmax(test_y, axis=1)
-    num_correct = np.sum(predicted_class == true_class) 
+    num_correct = np.sum(predicted_class == true_class)
     accuracy = float(num_correct)/result.shape[0]
     return (accuracy * 100)
 
 
 
-# Define the model
-model = Sequential()
-model.add(Convolution2D(64, (3, 3), padding='same', input_shape=(3, 32, 32)))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Convolution2D(96, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Convolution2D(96, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(256))
-model.add(Activation('tanh'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+
+
+'''
+Load Saved Model
+'''
+import keras
+
+with open('model-1.json', 'r') as myfile:
+    loaded_model_json = myfile.readlines()[0]
+
+from keras.models import model_from_json
+
+model = model_from_json(loaded_model_json)
+# load weights into new model
+model.load_weights("model-1.h5")
+print("Loaded model from disk")
+
+
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 # Train the model
 start = time.time()
-model_info = model.fit(train_features, train_labels, 
-                       batch_size=32, epochs=20, 
-                       validation_data = (test_features, test_labels), 
+model_info = model.fit(train_features, train_labels,
+                       batch_size=32, epochs=2,
+                       validation_data = (test_features, test_labels),
                        verbose=1)
 end = time.time()
 # plot model history
@@ -141,10 +103,11 @@ print("Accuracy on test data is: %0.2f"%accuracy(test_features, test_labels, mod
 print('saving model')
 # serialize model to JSON
 model_json = model.to_json()
-with open("model-1.json", "w+") as json_file:
+with open("model-1-end.json", "w+") as json_file:
     json_file.write(model_json)
-    
+
 # serialize weights to HDF5
-model.save_weights("model-1.h5")
+model.save_weights("model-1-end.h5")
 print("Saved model to disk")
+
 
